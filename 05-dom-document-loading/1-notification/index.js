d68c5192f15673af244538f32c0466b67710eb83
Zharikov.EI <zharikov.ei@gazprom-neft.ru>
2022-12-14 17:05:57 +0300

    add notification, sortTable1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
 
+  static activeNotification;
+
+  element;
+  timerId;
+
+  constructor(msg = '', {duration = 2000, type = 'success'} = {}) {
+    this.msg = msg;
+    this.duration = duration;
+    this.type = type;
+    this.durationSec = (duration / 1000) + 's';
+
+    this.render();
+  }
+
+  get template() {
+    return `<div class="notification ${this.type}" style="--value:${this.durationSec}">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">success</div>
+      <div class="notification-body">
+        ${this.msg}
+      </div>
+    </div>
+  </div>`
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  show(parent = document.body) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+    parent.append(this.element);
+    this.timerId = setTimeout(() => { this.remove(); }, this.duration)
+    NotificationMessage.activeNotification = this;
+  }
+
+  remove() {
+    clearTimeout(this.timerId);
+    if(this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    NotificationMessage.activeNotification = null;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,137 @@
-export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+import {template} from "@babel/core";
 
+export default class SortableTable {
+
+  element;
+  subElements = {};
+
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+
+    this.render();
+  }
+
+  getTableHeader() {
+    return `<div data-element="header" class="sortable-table__header sortable-table__row">
+                ${this.headerConfig.map(item => this.getHeaderRow(item)).join('')}
+              </div>`;
+  }
+
+  getHeaderRow({id, title, sortable}) {
+    return `
+    <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}">
+        <span>${title}</span>
+        <span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>
+      </div>
+    `;
+  }
+
+  getTableBody() {
+    return `
+    <div data-element="body" class="sortable-table__body">
+    ${this.getTableRows(this.data)}
+    </div>
+    `;
+  }
+
+  getTableRows(data = []) {
+    return data.map(item => {
+      return `
+    <a href="/products/${item.id}" class="sortable-table__row">
+      ${this.getTableRow(item)}
+      </a>
+    `;
+    });
+
+  }
+
+  getTableRow(el) {
+    const cells = this.headerConfig.map(({id, template}) => {
+      return {
+        id,
+        template
+      };
+    });
+    return cells.map(({id, template}) => {
+      return template ? template(el[id]) : `<div class="sortable-table__cell">${item[id]}</div>`;
+    }).join('');
+  }
+
+  getTable() {
+    return `
+    <div class="sortable-table">
+    ${this.getTableHeader()}
+    ${this.getTableBody()}
+    `;
+  }
+
+  render () {
+    const container = document.createElement('div');
+    container.innerHTML = this.getTable();
+    const element = container.firstElementChild;
+    this.element = element;
+    this.subElements = this.getSubElements(element);
+  }
+
+  sort(field, order) {
+    const sortedData = this.sortData(field, order);
+    const allCol = this.element.querySelectorAll('.sortable-table__cell[data-id]');
+    const curCol = this.element.querySelector('.sortable-table__cell[data-id=${field}]');
+    allCol.forEach(col => {
+      col.dataset.oreder = '';
+    });
+
+    curCol.dataset.oreder = order;
+
+    this.subElements.body.innerHTML = this.getTableRows(sortedData);
+  }
+
+  sortData(field, order) {
+    const arr = [...this.data];
+    const col = this.headerConfig.find(item => item.id === field);
+    const {sortType} = col;
+    const directions = {
+      asc: 1,
+      desc: -1
+    };
+    const direction = directions[order];
+
+    return arr.sort((a, b) => {
+      switch (sortType) {
+      case 'number': return direction * (a[field] - b[field]);
+      case 'string': return direction * a[field].localeCompare(b[field], ['ru', 'en']);
+      default:
+        throw new Error(`Invalid sort type - ${sortType}`);
+      }
+    });
+
+  }
+
+  getSubElements(element) {
+    const res = {};
+    const elements = element.querySelectorAll('[data-element]');
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+
+      res[name] = subElement;
+    }
+
+    return res;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
   }
 }
 
