870a561ac4406cf6b833288bd117e85acdbb15f2
Zharikov.EI <zharikov.ei@gazprom-neft.ru>
2022-12-15 13:42:24 +0300

    add tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,73 @@
 class Tooltip {
-  initialize () {
 
+  static instance;
+  element;
+
+
+  onPointerOver(event) {
+    const element = event.target.closest('[data-tooltip]');
+
+    if (element) {
+      this.render(element.dataset.tooltip);
+      document.addEventListener('pointermove', this.onPointerMove);
+    }
   }
+
+  onPointerMove(event) {
+    this.moveTooltip(event);
+  }
+
+  onPointerOut() {
+    this.remove();
+    document.removeEventListener('pointermove', this.onPointerMove);
+  }
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+    Tooltip.instance = this;
+  }
+
+  initialize () {
+    this.initListners();
+  }
+
+  initListners () {
+    document.addEventListener('poinertover', this.onPointerOver);
+    document.addEventListener('poinertout', this.onPointerOut);
+  }
+
+  render(html) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = html;
+
+    document.body.append(this.element);
+  }
+
+  moveTooltip(event) {
+    const shift = 10;
+    const left = event.clientX + shift;
+    const top = event.clientY + shift;
+    this.element.style.left = left + 'px';
+    this.element.style.top = top + 'px';
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    document.removeEventListener('poinertover', this.onPointerOver);
+    document.removeEventListener('poinertout', this.onPointerOut);
+    document.removeEventListener('poinertmove', this.onPointerMove);
+    this.remove();
+    this.element = null;
+  }
+
 }
 
 export default Tooltip;
